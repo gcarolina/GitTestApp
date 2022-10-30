8dd93b061955349b5fcc7670a14d98cf40ca7166
gcarolina <karolinaganizoda@gmail.com>
2022-10-30 23:13:55 +0300

    dev2-2

--- GitTestApp/GitTestApp/ViewController.swift
+++ GitTestApp/GitTestApp/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("я второй, но не последний")
+        print("что-то от второго")
     }
 
 
