e47af52145e17eeea5d99694200a2b0d1c1514fd
gcarolina <karolinaganizoda@gmail.com>
2022-10-30 22:33:41 +0300

    Project created

---
+++ GitTestApp/GitTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTestApp
+//
+//  Created by Carolina on 30.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
